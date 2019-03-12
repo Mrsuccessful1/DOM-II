ac6957cd42710ee8b867865ec86c200f9e2241ad
Mrsuccessful1 <mrsuccessfuldev@gmail.com>
2019-03-12 16:51:10 -0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// Your code goes here
+
+
+let bus = document.querySelector('.logo-heading');
+bus.addEventListener('mouseover', function(){ 
+bus.style.color = 'orange'; 
+});
+
+document.addEventListener('keydown', logKey);
+function logKey(e) {
+log.textContent += ` ${e.code}`;
+}
+
+var syntheticEvent = new WheelEvent("syntheticWheel", {"deltaX": 4, "deltaMode": 0});
+console.log(syntheticEvent.deltaX);
+
+
+var script = document.createElement("script");
+  script.addEventListener("load", function(event) {
+    console.log("Alll abooooaaard!!!!!");
+});
+script.src = "js/index.js";
+script.async = true;
+document.getElementsByTagName("script")[0].parentNode.appendChild(script);
+
+
